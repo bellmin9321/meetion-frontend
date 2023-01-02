b75e8c43956d491cdb20b90f09b48ddc46414c7b
musk <cpt@cashmallow.com>
2023-01-03 00:53:57 +0900

    🚚 Rename: jsx로 확장자 변경 및 불필요한 파일 삭제

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <button>Motion dfadfa</button>
+    </div>
+  );
+}
+
+export default App;
