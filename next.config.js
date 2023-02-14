15cc338354e30797a61a994ba6eba757c454d3ce
JONGMIN KIM <bellmin9321@gmail.com>
2023-02-15 03:28:31 +0900

    ✅ Add: next config 설정 변경

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
+  eslint: {
+    dirs: ['src'],
   },
+
+  reactStrictMode: true,
+  swcMinify: true,
 }
 
 module.exports = nextConfig
