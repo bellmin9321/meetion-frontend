474cdadc308a94a5e5afa84b7c2460f47367ccd2
JONGMIN KIM <bellmin9321@gmail.com>
2023-02-16 03:40:57 +0900

    ✅ Add: Header 컴포넌트 생성

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,22 @@
 import * as React from 'react';
+import { AiOutlineStar } from 'react-icons/ai';
+import { BiMessageDetail, BiTime } from 'react-icons/bi';
+import { FiMoreHorizontal } from 'react-icons/fi';
 
 export default function Header() {
-  return <header className="sticky top-0 z-50 bg-white">Meetion</header>;
+  return (
+    <header>
+      <div className="container flex flex-col flex-wrap items-center p-5 md:flex-row">
+        <nav className="flex flex-wrap items-center justify-center text-base md:ml-auto">
+          <span className="mr-3 cursor-pointer rounded-md py-1 px-2 hover:bg-gray-200 ">
+            공유
+          </span>
+          <BiMessageDetail className="icon mr-5 cursor-pointer rounded-md hover:bg-gray-200" />
+          <BiTime className="icon mr-5 cursor-pointer rounded-md hover:bg-gray-200" />
+          <AiOutlineStar className="icon mr-5 cursor-pointer rounded-md hover:bg-gray-200" />
+          <FiMoreHorizontal className="icon mr-5 cursor-pointer rounded-md hover:bg-gray-200" />
+        </nav>
+      </div>
+    </header>
+  );
 }
