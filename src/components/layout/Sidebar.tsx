751c5c2b474b84541b872019490e47e0ae61c3f0
JONGMIN KIM <bellmin9321@gmail.com>
2023-02-21 00:23:29 +0900

    🎨 Style: 사이드 바 내 리스트 스타일 수정

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import React, { ReactNode } from 'react';
-import { BiSearch, BiTime } from 'react-icons/bi';
+import { BiPlus, BiSearch, BiTime } from 'react-icons/bi';
 import { FaRegSun } from 'react-icons/fa';
 
 import List from '../List/List';
@@ -18,37 +18,36 @@ function Sidebar() {
   ];
 
   return (
-    <nav>
-      <aside
-        id="sidebar-multi-level-sidebar"
-        className="fixed top-0 left-0 z-40 h-screen w-64 -translate-x-full transition-transform sm:translate-x-0"
-        aria-label="Sidebar"
-      >
+    <aside
+      id="sidebar-multi-level-sidebar"
+      className="fixed top-0 left-0 z-40 h-screen w-60 -translate-x-full transition-transform sm:translate-x-0"
+      aria-label="Sidebar"
+    >
+      <div className="h-full overflow-y-auto bg-gray-100 py-4 dark:bg-gray-800">
         <Link
           href="http://localhost:3000/"
           className="mb-5 flex justify-center"
         >
           <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-            bellmin님의 meetion
+            Meetion
           </span>
         </Link>
-        <div className="h-full overflow-y-auto bg-gray-50 px-3 py-4 dark:bg-gray-800">
-          <ul className="space-y-2">
+        <div>
+          <ul className="mb-8 space-y-1">
             {categories.map((category: CategoryType, index: number) => {
               return <List key={index} category={category} />;
             })}
           </ul>
-          <div>공유된 페이지</div>
-          <ul className="space-y-2">
-            {categories.map((category: CategoryType, index: number) => {
-              return <List key={index} category={category} />;
-            })}
-          </ul>
-          <div>개인 페이지</div>
-          <div>새 페이지</div>
+          <div className="ml-4">개인 페이지</div>
+          <div className="fixed bottom-0 flex w-full cursor-pointer border-t-[1px] py-4 hover:bg-gray-200">
+            <span className="ml-4">
+              <BiPlus className="mr-3 h-6 w-6 text-gray-500" />
+            </span>
+            <span className="text-gray-500">새 페이지</span>
+          </div>
         </div>
-      </aside>
-    </nav>
+      </div>
+    </aside>
   );
 }
 
