29f4cfad33db39cabad28b37e90fefbfc1f01658
JONGMIN KIM <bellmin9321@gmail.com>
2023-02-21 00:17:37 +0900

    🌳 Refactor: 공통 css 적용 및 input 기본 설정

--- src/components/layout/Content.tsx
+++ src/components/layout/Content.tsx
@@ -1,22 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './Header';
 
 const Content = () => {
+  const [title, setTitle] = useState<string>('');
+  const [desc, setDesc] = useState<string>('');
+
   return (
     <>
       <Header />
       <div className="p-4 sm:ml-64">
         <textarea
           id="message"
-          className="text-3lg mb-5 block w-4/5 border-none bg-gray-50 p-2.5 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+          className="textarea text-3lg mb-5 "
           placeholder="제목 없음"
+          onChange={(e) => setTitle(e.target.value)}
         />
 
         <textarea
           id="message"
-          className="block w-4/5 border-none bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+          className="textarea block w-4/5 text-sm"
           placeholder="'/'를 입력해 명령어를 사용하세요"
+          onChange={(e) => setDesc(e.target.value)}
         />
       </div>
     </>
