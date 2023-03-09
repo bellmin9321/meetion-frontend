67cda9b1b428b5e3b646256834ee087fe47d77e3
JONGMIN KIM <bellmin9321@gmail.com>
2023-03-10 03:16:39 +0900

    ✨ Feat: 페이지 route 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ export default function HomePage() {
       </Head>
       <Layout>
         <Sidebar />
-        <Content />
+        <Content page={undefined} />
       </Layout>
     </>
   );
---
+++ src/pages/page/[id].tsx
@@ -0,0 +1,25 @@
+import { useRouter } from 'next/router';
+import { useRecoilValue } from 'recoil';
+
+import Content from '@/components/layout/Content';
+import Layout from '@/components/layout/Layout';
+import Sidebar from '@/components/layout/Sidebar';
+
+import { pageList } from '@/recoil';
+
+const Page = () => {
+  const router = useRouter();
+  const { id } = router.query;
+  const pages = useRecoilValue(pageList);
+
+  const target = pages.find((page) => page._id === id);
+
+  return (
+    <Layout>
+      <Sidebar />
+      <Content page={target} />
+    </Layout>
+  );
+};
+
+export default Page;
