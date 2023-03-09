f0a80301612b4e9050dbd65dae421a859b83fad9
JONGMIN KIM <bellmin9321@gmail.com>
2023-03-10 01:11:26 +0900

    🎨 Style: tailwind global css로 변경

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -8,14 +8,9 @@ interface ListProp {
 
 function List({ category }: ListProp) {
   return (
-    <li>
-      <a
-        href="#"
-        className="rounded-m flex items-center py-1 text-base font-normal text-gray-500 hover:bg-gray-200 dark:text-white dark:hover:bg-gray-700"
-      >
-        <span className="ml-4">{category.icon}</span>
-        <span className="ml-2">{category.name}</span>
-      </a>
+    <li className="toolList">
+      <span className="ml-4">{category.icon}</span>
+      <span className="ml-2">{category.name}</span>
     </li>
   );
 }
