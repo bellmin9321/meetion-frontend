46bed1c1f9352b1bc8ff5c2d7bba7c95df8838fc
JONGMIN KIM <bellmin9321@gmail.com>
2023-03-10 16:51:13 +0900

    ✨ Feat: page 삭제 기능 구현

--- src/components/layout/Sidebar/MyPage.tsx
+++ src/components/layout/Sidebar/MyPage.tsx
@@ -1,10 +1,14 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React, { useEffect } from 'react';
 import { AiFillDelete, AiOutlineRight } from 'react-icons/ai';
 import { useQuery } from 'react-query';
 import { useSetRecoilState } from 'recoil';
 
+import usePageMutation from '@/hooks/usePageMutation';
+
 import { getPageList } from '@/api/page';
+import { queryClient } from '@/api/queryClient';
 import { pageList } from '@/recoil';
 
 import { PageType } from '@/types';
@@ -12,11 +16,28 @@ import { PageType } from '@/types';
 function MyPage() {
   const { data } = useQuery('pages', getPageList);
   const setPageList = useSetRecoilState(pageList);
+  const { removePage } = usePageMutation();
+  const { mutate: deletePageMutate } = removePage;
+  const router = useRouter();
 
   useEffect(() => {
     setPageList(data);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const handleDelete = (id: string) => {
+    console.log('delete', id);
+    deletePageMutate(id, {
+      onSuccess: () => {
+        queryClient.invalidateQueries('pages');
+        router.push('/');
+      },
+      onError: (error, variable, context) => {
+        console.log(error);
+      },
+    });
+  };
+
   return (
     <>
       <div className="ml-4 mb-2 text-sm text-gray-500">개인 페이지</div>
@@ -24,16 +45,21 @@ function MyPage() {
         {data?.length > 0 &&
           data.map((page: PageType) => {
             return (
-              <div key={page._id} className="flex justify-between">
+              <div key={page._id} className="mb-1 flex justify-between">
                 <Link href={`/page/${page._id}`}>
-                  <div className="mb-1 flex items-center">
+                  <div className="flex items-center">
                     <span>
                       <AiOutlineRight className="text-sm  text-gray-600" />
                     </span>
                     <li className="ml-2 text-gray-600">{page?.title}</li>
                   </div>
                 </Link>
-                <AiFillDelete className="mr-4 cursor-pointer hover:text-red-500" />
+                <div className="flex items-center">
+                  <AiFillDelete
+                    className="mr-4 flex cursor-pointer items-center hover:text-red-500"
+                    onClick={() => handleDelete(page._id)}
+                  />
+                </div>
               </div>
             );
           })}
