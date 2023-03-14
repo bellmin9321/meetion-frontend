d6694573616dafe28e78add12260f4702d1eafcf
JONGMIN KIM <bellmin9321@gmail.com>
2023-03-14 17:25:22 +0900

    ✅ Add: page edit api 추가

--- src/lib/hooks/usePageMutation.ts
+++ src/lib/hooks/usePageMutation.ts
@@ -1,13 +1,15 @@
 import { useMutation } from 'react-query';
 
-import { createNewPage, deletePage } from '@/lib/api/page';
+import { createNewPage, deletePage, patchPage } from '@/lib/api/page';
 
 export default function usePageMutation() {
   const addPage = useMutation(createNewPage);
   const removePage = useMutation(deletePage);
+  const editPage = useMutation(patchPage);
 
   return {
     addPage,
     removePage,
+    editPage,
   };
 }
