91c008352c120b61381d3b7296c2a57c41cc9caa
JONGMIN KIM <bellmin9321@gmail.com>
2023-03-14 18:01:14 +0900

    ✅ Add: param에 숫자도 추가

--- src/lib/service/index.ts
+++ src/lib/service/index.ts
@@ -1,5 +1,5 @@
 export const changeParam = (param: string) => {
-  const engReg = /[^a-z|A-Z]/g;
+  const engReg = /[^a-z|A-Z][0-9]/g;
   const SpaceToHyphenReg = /\s|_/g;
   const newParam = param.replace(engReg, '').replace(SpaceToHyphenReg, '-');
 
