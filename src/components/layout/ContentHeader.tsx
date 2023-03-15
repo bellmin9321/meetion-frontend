d7848403ab61bb116ca8bac286b43a9de800cdc3
JONGMIN KIM <bellmin9321@gmail.com>
2023-03-15 23:30:01 +0900

    ✨ Feat: logout 버튼 추가

--- src/components/layout/ContentHeader.tsx
+++ src/components/layout/ContentHeader.tsx
@@ -1,13 +1,24 @@
+import { signOut } from 'next-auth/react';
 import * as React from 'react';
 import { AiOutlineStar } from 'react-icons/ai';
 import { BiMessageDetail, BiTime } from 'react-icons/bi';
 import { FiMoreHorizontal } from 'react-icons/fi';
 
 export default function Header() {
+  const handleLogout = () => {
+    signOut();
+  };
+
   return (
     <header>
       <div className="flex flex-col flex-wrap items-center p-5 md:flex-row">
         <nav className="flex flex-wrap items-center justify-center text-base md:ml-auto">
+          <span
+            className="mr-3 cursor-pointer rounded-md py-1 px-2 hover:bg-gray-200 "
+            onClick={handleLogout}
+          >
+            로그아웃
+          </span>
           <span className="mr-3 cursor-pointer rounded-md py-1 px-2 hover:bg-gray-200 ">
             공유
           </span>
