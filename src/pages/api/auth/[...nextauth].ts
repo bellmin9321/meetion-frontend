09b8870a9aef15435cb97c35c2a45205b356769e
JONGMIN KIM <bellmin9321@gmail.com>
2023-03-17 00:38:34 +0900

    ✅ Add: Google, Github, Kakao, Naver  Provider

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,35 @@
+import NextAuth from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+import KakaoProvider from 'next-auth/providers/kakao';
+import NaverProvider from 'next-auth/providers/naver';
+
+export default NextAuth({
+  callbacks: {
+    session({ session }) {
+      return session;
+    },
+  },
+  session: {
+    strategy: 'jwt',
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    }),
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID as string,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET as string,
+    }),
+    NaverProvider({
+      clientId: process.env.NAVER_CLIENT_ID as string,
+      clientSecret: process.env.NAVER_CLIENT_SECRET as string,
+    }),
+  ],
+  secret: process.env.JWT_SECRET as string,
+});
