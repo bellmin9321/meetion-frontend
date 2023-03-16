bf849425d2d8a3c4f0f135533050a5a7629c5cc8
JONGMIN KIM <bellmin9321@gmail.com>
2023-03-17 02:36:46 +0900

    🌳 Refactor: use Login buttons by map func

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,12 +1,8 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { NextPageContext } from 'next';
 import Head from 'next/head';
 import { getProviders, getSession, signIn } from 'next-auth/react';
-import { AiFillGithub } from 'react-icons/ai';
-import { FcGoogle } from 'react-icons/fc';
-import { RiKakaoTalkFill } from 'react-icons/ri';
-import { SiNaver } from 'react-icons/si';
+
+import { LoginProperties } from '@/lib/recoil';
 
 import { LoginProp } from '@/types';
 
@@ -25,34 +21,20 @@ function Login({ providers }: { providers: LoginProp }) {
             로그인
           </div>
           <div className="flex w-[350px] flex-col justify-center ">
-            <button
-              className="loginBtn"
-              onClick={() => signIn(providers!.google.id)}
-            >
-              <FcGoogle className="mr-2 h-5 w-5" />
-              Goolgle로 계속하기
-            </button>
-            <button
-              className="loginBtn bg-gray-800 text-white"
-              onClick={() => signIn(providers!.github.id)}
-            >
-              <AiFillGithub className="mr-2 h-5 w-5" />
-              Github으로 계속하기
-            </button>
-            <button
-              className="loginBtn bg-yellow-300"
-              onClick={() => signIn(providers!.kakao.id)}
-            >
-              <RiKakaoTalkFill className="mr-2 h-5 w-5" />
-              Kakao로 계속하기
-            </button>
-            <button
-              className="loginBtn bg-green-500 text-white"
-              onClick={() => signIn(providers!.naver.id)}
-            >
-              <SiNaver className="mr-2 h-5 w-5" />
-              Naver로 계속하기
-            </button>
+            {Object.values(providers).map((provider, i) => {
+              const Icon = LoginProperties[i].icon;
+
+              return (
+                <button
+                  key={provider.id}
+                  className={`loginBtn + ${LoginProperties[i].style}`}
+                  onClick={() => signIn(provider.id)}
+                >
+                  <Icon className="mr-2 h-5 w-5" />
+                  <span>{provider.name}로 계속하기</span>
+                </button>
+              );
+            })}
           </div>
         </div>
       </div>
