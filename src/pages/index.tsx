aeadf28a7e66afe2c9fe0aa3979c2d6700d47956
JONGMIN KIM <bellmin9321@gmail.com>
2023-03-17 00:47:18 +0900

    ✨ Feat: 초기 렌더링 시 page, user api 요청 및 recoil에 상태 저장

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,59 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { NextPageContext } from 'next';
+import { getProviders, getSession, useSession } from 'next-auth/react';
 import React from 'react';
+import { useQuery } from 'react-query';
+import { useSetRecoilState } from 'recoil';
+
+import { getPageList } from '@/lib/api/page';
+import useUserMutation from '@/lib/hooks/useUserMutation';
+import { pageList, userState } from '@/lib/recoil';
 
 import Content from '@/components/layout/Content';
 import Layout from '@/components/layout/Layout';
 
-export default function HomePage() {
+import { queryKeys } from '@/types/commonType';
+
+function HomePage() {
+  const { data: session } = useSession();
+  const setUser = useSetRecoilState(userState);
+  const setPages = useSetRecoilState(pageList);
+  const { addUser } = useUserMutation();
+
+  const { mutate: addUserMutate } = addUser;
+  useQuery([...queryKeys.pages, session], async () => {
+    if (session) {
+      const data = await getPageList(session.user.email);
+      setUser(session.user);
+      addUserMutate(session.user);
+      setPages(data);
+    }
+  });
+
   return (
     <Layout>
       <Content page={undefined} />
     </Layout>
   );
 }
+
+export async function getServerSideProps(context: NextPageContext) {
+  const { req, res } = context;
+  const session = await getSession({ req });
+
+  if (!session && res) {
+    res.writeHead(302, {
+      Location: '/login',
+    });
+    res.end();
+    return;
+  }
+
+  return {
+    props: {
+      providers: await getProviders(),
+    },
+  };
+}
+
+export default HomePage;
