391193a899e5a131091f65f11afd796176ff291d
JONGMIN KIM <bellmin9321@gmail.com>
2023-03-17 02:36:46 +0900

    🌳 Refactor: use Login buttons by map func

--- src/lib/recoil/index.ts
+++ src/lib/recoil/index.ts
@@ -1,7 +1,19 @@
 import { atom, RecoilEnv } from 'recoil';
 RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = false;
 
+import { AiFillGithub } from 'react-icons/ai';
+import { FcGoogle } from 'react-icons/fc';
+import { RiKakaoTalkFill } from 'react-icons/ri';
+import { SiNaver } from 'react-icons/si';
+
 import { PageType, UserType } from '@/types';
+
+export const LoginProperties = [
+  { icon: FcGoogle, style: '' },
+  { icon: AiFillGithub, style: 'bg-gray-800 text-white' },
+  { icon: RiKakaoTalkFill, style: 'bg-yellow-300' },
+  { icon: SiNaver, style: 'bg-green-500 text-white' },
+];
 
 export const pageState = atom<PageType>({
   key: 'page',
