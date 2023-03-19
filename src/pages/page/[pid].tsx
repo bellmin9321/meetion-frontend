6a408965384c7dc943251ac1b1d9fa0852040c19
JONGMIN KIM <bellmin9321@gmail.com>
2023-03-20 00:03:51 +0900

    ✅ Add: Link 페이지 SSR 적용

--- src/pages/page/[pid].tsx
+++ src/pages/page/[pid].tsx
@@ -1,16 +1,20 @@
+import { NextPageContext } from 'next';
 import { useRouter } from 'next/router';
+import { getSession } from 'next-auth/react';
 import { useRecoilValue } from 'recoil';
 
-import { pageList } from '@/lib/recoil';
+import useHomePage from '@/lib/hooks/useHomePage';
+import { pageListState } from '@/lib/recoil';
 
 import Content from '@/components/layout/Content';
 import Layout from '@/components/layout/Layout';
 
 const Page = () => {
+  useHomePage();
   const router = useRouter();
 
   const { pid } = router.query;
-  const pages = useRecoilValue(pageList);
+  const pages = useRecoilValue(pageListState);
 
   let originalId: string;
 
@@ -19,13 +23,37 @@ const Page = () => {
     originalId = arr[arr.length - 1];
   }
 
-  const target = pages.find((page) => page._id === originalId);
+  const page = pages.find((page) => page._id === originalId);
 
   return (
     <Layout>
-      <Content page={target} />
+      <Content page={page} />
     </Layout>
   );
 };
+
+export async function getServerSideProps(context: NextPageContext) {
+  try {
+    const { req, res } = context;
+    const session = await getSession({ req });
+
+    if (!session && res) {
+      res.writeHead(302, {
+        Location: '/login',
+      });
+      res.end();
+      return { props: {} };
+    }
+
+    return { props: session };
+  } catch (error) {
+    return {
+      redirect: {
+        destination: '/login',
+        statusCode: 302,
+      },
+    };
+  }
+}
 
 export default Page;
