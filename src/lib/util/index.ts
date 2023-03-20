c5873cbf404f7bd718327e48d18d39f1c9a6ec03
JONGMIN KIM <bellmin9321@gmail.com>
2023-03-21 00:00:49 +0900

    🚚 Rename: sevice -> util 폴더명 변경

--- src/lib/util/index.ts
+++ src/lib/util/index.ts
@@ -1,7 +1,8 @@
 export const changeParam = (param: string) => {
-  const engReg = /[^a-z|A-Z][0-9]/g;
+  const engReg = /[^a-z|A-Z|0-9]/g;
   const SpaceToHyphenReg = /\s|_/g;
   const newParam = param.replace(engReg, '').replace(SpaceToHyphenReg, '-');
+  console.log('newParam: ', newParam);
 
   return newParam ? newParam + '-' : '';
 };
