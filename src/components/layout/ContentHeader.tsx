10032471f870f4612beed3535bdb16248fd3d22a
JONGMIN KIM <bellmin9321@gmail.com>
2023-03-22 00:00:22 +0900

    💄 Design: 페이지 제목 삽입 및 구조 변경

--- src/components/layout/ContentHeader.tsx
+++ src/components/layout/ContentHeader.tsx
@@ -4,30 +4,31 @@ import { AiOutlineStar } from 'react-icons/ai';
 import { BiMessageDetail, BiTime } from 'react-icons/bi';
 import { FiMoreHorizontal } from 'react-icons/fi';
 
-export default function Header() {
+function ContentHeader({ title }: { title: string }) {
   const handleLogout = () => {
     signOut();
   };
 
   return (
-    <header>
-      <div className="flex flex-col flex-wrap items-center p-5 md:flex-row">
-        <nav className="flex flex-wrap items-center justify-center text-base md:ml-auto">
-          <span
-            className="mr-3 cursor-pointer rounded-md py-1 px-2 hover:bg-gray-200 "
-            onClick={handleLogout}
-          >
-            로그아웃
-          </span>
-          <span className="mr-3 cursor-pointer rounded-md py-1 px-2 hover:bg-gray-200 ">
-            공유
-          </span>
-          <BiMessageDetail className="icon mr-5 cursor-pointer rounded-md hover:bg-gray-200" />
-          <BiTime className="icon mr-5 cursor-pointer rounded-md hover:bg-gray-200" />
-          <AiOutlineStar className="icon mr-5 cursor-pointer rounded-md hover:bg-gray-200" />
-          <FiMoreHorizontal className="icon mr-5 cursor-pointer rounded-md hover:bg-gray-200" />
-        </nav>
+    <header className="fixed top-0 left-0 right-0 flex flex-row flex-wrap items-center p-3 md:flex-row">
+      <div className="ml-[250px]">{title}</div>
+      <div className="flex flex-wrap items-center justify-center  text-base md:ml-auto">
+        <span
+          className="mr-3 cursor-pointer rounded-md px-2 hover:bg-gray-200 "
+          onClick={handleLogout}
+        >
+          로그아웃
+        </span>
+        <span className="mr-3 cursor-pointer rounded-md px-2 hover:bg-gray-200 ">
+          공유
+        </span>
+        <BiMessageDetail className="icon mr-5 cursor-pointer rounded-md hover:bg-gray-200" />
+        <BiTime className="icon mr-5 cursor-pointer rounded-md hover:bg-gray-200" />
+        <AiOutlineStar className="icon mr-5 cursor-pointer rounded-md hover:bg-gray-200" />
+        <FiMoreHorizontal className="icon mr-5 cursor-pointer rounded-md hover:bg-gray-200" />
       </div>
     </header>
   );
 }
+
+export default ContentHeader;
