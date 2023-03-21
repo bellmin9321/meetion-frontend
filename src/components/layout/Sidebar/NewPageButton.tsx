797caed977b07632291fc2b733b7f7e1e975aa0f
JONGMIN KIM <bellmin9321@gmail.com>
2023-03-21 23:02:12 +0900

    ✨ Feat: 새 페이지 클릭 시 제목 없음 페이지 추가

--- src/components/layout/Sidebar/NewPageButton.tsx
+++ src/components/layout/Sidebar/NewPageButton.tsx
@@ -1,19 +1,51 @@
-import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React from 'react';
 import { BiPlus } from 'react-icons/bi';
+import { useRecoilValue } from 'recoil';
 
-function NewPage() {
+import { queryClient } from '@/lib/api/queryClient';
+import usePageMutation from '@/lib/hooks/usePageMutation';
+import { userState } from '@/lib/recoil';
+
+import { queryKeys } from '@/types/commonType';
+
+function NewPageButton() {
+  const { email } = useRecoilValue(userState);
+  const { addPage } = usePageMutation();
+  const { mutate: addPageMutate } = addPage;
+  const router = useRouter();
+
+  const page = {
+    creator: email,
+    title: '',
+    desc: '',
+  };
+
+  const handleClick = () => {
+    addPageMutate(page, {
+      onSuccess: (data) => {
+        queryClient.invalidateQueries(queryKeys.pages);
+        if (data) {
+          router.push(`/page/${data._id}`);
+        }
+      },
+      onError: (error) => {
+        console.log(error);
+      },
+    });
+  };
+
   return (
-    <Link
-      href="/"
+    <div
       className="fixed bottom-0 flex w-full cursor-pointer border-t-[1px] py-4 hover:bg-gray-200"
+      onClick={handleClick}
     >
       <span className="ml-4">
         <BiPlus className="mr-3 h-6 w-6 text-gray-500" />
       </span>
       <span className="text-gray-500">새 페이지</span>
-    </Link>
+    </div>
   );
 }
 
-export default NewPage;
+export default NewPageButton;
