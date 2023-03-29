88c444853a88b7a7e6318d3c9ac03f83034c7e12
JONGMIN KIM <bellmin9321@gmail.com>
2023-03-29 16:22:56 +0900

    ✅ Add: image/next 허용 이미지 설정 추가

--- next.config.js
+++ next.config.js
@@ -9,6 +9,22 @@ const nextConfig = {
   env: {
     LOCAL_BASE_URL: process.env.LOCAL_BASE_URL,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/a/AGNmyxYqqEdCdDDi7YxJOpWQ8ySyBO_tlTh5fqZgHQUAdQ=s96-c',
+      },
+      {
+        protocol: 'https',
+        hostname: 'phinf.pstatic.net',
+        port: '',
+        pathname: '/contact/20200309_52/1583734886909vxeE7_JPEG/profileImage.jpg',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
