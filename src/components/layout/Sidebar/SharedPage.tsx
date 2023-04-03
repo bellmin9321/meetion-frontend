f81dcf6c09c97cef040b522e7926c6fa509ef924
JONGMIN KIM <bellmin9321@gmail.com>
2023-04-03 19:02:03 +0900

    ✨ Feat: 본인이 공유한 페이지의 경우에만 삭제 가능 기능 추가

--- src/components/layout/Sidebar/SharedPage.tsx
+++ src/components/layout/Sidebar/SharedPage.tsx
@@ -1,19 +1,43 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React from 'react';
-import { AiOutlineRight } from 'react-icons/ai';
+import { AiFillDelete, AiOutlineRight } from 'react-icons/ai';
 import { useRecoilState, useRecoilValue } from 'recoil';
 
-import { selectedPageID, sharedPagesState } from '@/lib/recoil';
+import { queryClient } from '@/lib/api/queryClient';
+import usePageMutation from '@/lib/hooks/usePageMutation';
+import { selectedPageID, sharedPagesState, userState } from '@/lib/recoil';
 import { changeParam, textLengthOverCut } from '@/lib/util';
 
 import { PageType } from '@/types';
+import { queryKeys } from '@/types/commonType';
 
 function SharedPage() {
   const sharedPages = useRecoilValue(sharedPagesState);
+  const { email } = useRecoilValue(userState);
   const [selectedId, setSelected] = useRecoilState(selectedPageID);
+  const { removePage } = usePageMutation();
+  const { mutate: deletePageMutate } = removePage;
+  const router = useRouter();
 
   const getSelectedClass = (id?: string) =>
     selectedId === id ? 'selectedList' : '';
+
+  const handleDelete = (id?: string) => {
+    deletePageMutate(id ?? '', {
+      onSuccess: () => {
+        const { _id, title } = sharedPages[0];
+
+        queryClient.invalidateQueries(queryKeys.pages);
+        router.push(`/page/${changeParam(title)}${_id}`, undefined, {
+          shallow: true,
+        });
+      },
+      onError: (error) => {
+        console.log(error);
+      },
+    });
+  };
 
   return sharedPages?.length ? (
     <div className="mb-6">
@@ -43,6 +67,15 @@ function SharedPage() {
                         : '제목 없음'}
                     </li>
                   </div>
+
+                  {email === page.creator && (
+                    <div className="flex items-center">
+                      <AiFillDelete
+                        className="mr-4 flex cursor-pointer items-center text-gray-600 hover:text-red-500"
+                        onClick={() => handleDelete(page._id)}
+                      />
+                    </div>
+                  )}
                 </Link>
               </div>
             );
