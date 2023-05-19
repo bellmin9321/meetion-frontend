7508e3bf139856c65d689d34e885da3f89889533
JONGMIN KIM <bellmin9321@gmail.com>
2023-05-19 22:26:41 +0900

    🐛 Fix: User interface 추가함으로써 CredentialProvider authorize type 에러 해결

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,9 +1,14 @@
+import { User as UserModel } from '@prisma/client';
 import { DefaultSession } from 'next-auth';
 
 declare module 'next-auth' {
   /**
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
+  interface User extends UserModel {
+    id: number;
+  }
+
   interface Session {
     user: {
       email: string;
