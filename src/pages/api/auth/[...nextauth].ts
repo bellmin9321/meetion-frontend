76fee063b863a25b8fabe1aee7b453d2bd2e5738
JONGMIN KIM <bellmin9321@gmail.com>
2023-05-22 02:39:10 +0900

    ✅ Add: credentialProvider 추가

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,61 @@
 import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 import KakaoProvider from 'next-auth/providers/kakao';
 import NaverProvider from 'next-auth/providers/naver';
+
+const providers = [
+  GoogleProvider({
+    clientId: process.env.GOOGLE_CLIENT_ID as string,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+  }),
+  GithubProvider({
+    clientId: process.env.GITHUB_CLIENT_ID as string,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+  }),
+  KakaoProvider({
+    clientId: process.env.KAKAO_CLIENT_ID as string,
+    clientSecret: process.env.KAKAO_CLIENT_SECRET as string,
+  }),
+  NaverProvider({
+    clientId: process.env.NAVER_CLIENT_ID as string,
+    clientSecret: process.env.NAVER_CLIENT_SECRET as string,
+  }),
+  CredentialsProvider({
+    name: 'Credentials',
+    credentials: {
+      email: {
+        label: 'email',
+        type: 'text',
+        placeholder: '아이디를 입력하세요',
+      },
+      password: { label: 'Password', type: 'password' },
+    },
+    async authorize(credentials) {
+      try {
+        if (!credentials) throw new Error('no credentials to log in as');
+
+        const { email, password } = credentials as {
+          email: string;
+          password: string;
+        };
+
+        if (email === 'bellmin9321@gmail.com' && password === 'cypress') {
+          return {
+            id: 1,
+            name: '김종민',
+            email: 'bellmin9321@gmail.com',
+          };
+        }
+
+        return null;
+      } catch (error) {
+        return null;
+      }
+    },
+  }),
+];
 
 export default NextAuth({
   callbacks: {
@@ -11,26 +64,9 @@ export default NextAuth({
     },
   },
   session: {
-    strategy: 'jwt',
+    strategy: process.env.NODE_ENV === 'production' ? 'database' : 'jwt',
   },
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-    GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
-    }),
-    KakaoProvider({
-      clientId: process.env.KAKAO_CLIENT_ID as string,
-      clientSecret: process.env.KAKAO_CLIENT_SECRET as string,
-    }),
-    NaverProvider({
-      clientId: process.env.NAVER_CLIENT_ID as string,
-      clientSecret: process.env.NAVER_CLIENT_SECRET as string,
-    }),
-  ],
+  providers,
   pages: {
     signIn: '/auth/signin',
     error: 'auth/error',
