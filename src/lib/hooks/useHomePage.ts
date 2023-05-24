3d1f8b50e9a5f19413615f8cca122dcde15a8a29
JONGMIN KIM <bellmin9321@gmail.com>
2023-05-24 16:10:48 +0900

    🌳 Refactor: useHomePage 훅을 이용한 리팩토링 및 초기 렌더링 시 첫 페이지로 라우팅 안되는 코드 수정

--- src/lib/hooks/useHomePage.ts
+++ src/lib/hooks/useHomePage.ts
@@ -1,7 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useRouter } from 'next/router';
 import { useSession } from 'next-auth/react';
-import { useEffect } from 'react';
 import { useQuery } from 'react-query';
 import { useRecoilState, useSetRecoilState } from 'recoil';
 
@@ -43,11 +42,7 @@ function useHomePage() {
     },
   );
 
-  useEffect(() => {
-    router.push(`/page/${(pages[0] || sharedPages[0])?._id}`, undefined, {
-      shallow: true,
-    });
-  }, []);
+  return { pages, sharedPages, router };
 }
 
 export default useHomePage;
